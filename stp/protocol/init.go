61aee832d66a61babde737b96ff5efc40f1c05b9
ccordes-snaproute <ccordes@snaproute.com>
2016-01-27 14:41:32 -0800

    fix for crash

--- stp/protocol/init.go
+++ stp/protocol/init.go
@@ -1,11 +1,17 @@
 // init.go
 package stp
 
-import ()
+import (
+	"log/syslog"
+)
+
+var gLogger *syslog.Writer
 
 func init() {
-
+	PortConfigMap = make(map[int32]portConfig)
 	TimerTypeStrStateMapInit()
 	PtmMachineStrStateMapInit()
 	PrxmMachineStrStateMapInit()
+
+	gLogger, _ = syslog.New(syslog.LOG_INFO|syslog.LOG_DAEMON, "STP")
 }
