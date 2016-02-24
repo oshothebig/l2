eecbd96c359e1a316e26a0c504b90cb824baa60b
ccordes-snaproute <ccordes@snaproute.com>
2016-02-23 17:48:38 -0800

    reverted some previous change

--- stp/protocol/pimfsm.go
+++ stp/protocol/pimfsm.go
@@ -200,7 +200,7 @@ func (pim *PimMachine) PimMachineUpdate(m fsm.Machine, data interface{}) fsm.Sta
 	}
 	p.PortPriority.DesignatedBridgeId = p.b.BridgeIdentifier
 	p.PortPriority.DesignatedPortId = uint16(p.Priority<<8 | p.PortId)
-	p.PortTimes = p.b.RootTimes
+	p.PortTimes = p.b.BridgeTimes
 	//defer p.NotifyUpdtInfoChanged(PimMachineModuleStr, p.UpdtInfo, false)
 	p.UpdtInfo = false
 	p.InfoIs = PortInfoStateMine
