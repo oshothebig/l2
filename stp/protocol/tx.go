fe27fffa0315d702f8a836c6cd43547b1ccea43b
ccordes-snaproute <ccordes@snaproute.com>
2016-02-29 15:18:56 -0800

    added length of pvst to dot1q header

--- stp/protocol/tx.go
+++ stp/protocol/tx.go
@@ -49,7 +49,7 @@ func (p *StpPort) TxPVST() {
 		Priority:       PVST_VLAN_PRIORITY,
 		DropEligible:   false,
 		VLANIdentifier: p.b.Vlan,
-		Type:           layers.PVSTProtocolLength + 3 + 5,
+		Type:           layers.EthernetType(layers.PVSTProtocolLength + 3 + 5), // length
 	}
 
 	llc := layers.LLC{
