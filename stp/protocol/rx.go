a5cae18d33efdf58b7e6002e369caafc78f8733e
ccordes-snaproute <ccordes@snaproute.com>
2016-03-01 08:59:25 -0800

    debug

--- stp/protocol/rx.go
+++ stp/protocol/rx.go
@@ -35,7 +35,7 @@ func BpduRxMain(pId int32, bId int32, rxPktChan chan gopacket.Packet) {
 						if p != nil {
 							//fmt.Println("RxMain: port", rxMainPort)
 							ptype := ValidateBPDUFrame(p, packet)
-							//fmt.Println("RX:", packet, ptype)
+							fmt.Println("RX:", packet, ptype)
 							if ptype != BPDURxTypeUnknown {
 								ProcessBpduFrame(p, ptype, packet)
 							}
