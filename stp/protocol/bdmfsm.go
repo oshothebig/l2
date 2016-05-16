334ea314c6040e96a71df785c1bd01532f682629
Hari Gollapalli <hgollapalli@snaproute.com>
2016-05-16 16:44:37 -0700

    Adding license text

--- stp/protocol/bdmfsm.go
+++ stp/protocol/bdmfsm.go
@@ -1,3 +1,16 @@
+Copyright [2016] [SnapRoute Inc]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+	 Unless required by applicable law or agreed to in writing, software
+	 distributed under the License is distributed on an "AS IS" BASIS,
+	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 See the License for the specific language governing permissions and
+	 limitations under the License.
 // 802.1D-2004 17.25 Bridge Detection State Machine
 //The Bridge Detection state machine shall implement the function specified by the state diagram in Figure
 //17-16, the definitions in 17.16, 17.13, 17.20, and 17.21, and the variable declarations in 17.17, 17.18, and
