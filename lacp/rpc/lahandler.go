5e2efc416216104a988193286eb50e288a734038
mjoshiSnaproute <mjoshi@Snaproute.com>
2016-05-25 15:38:33 -0700

    modified:   lacp/rpc/lahandler.go 	modified:   lldp/flexswitch/listener.go 	modified:   stp/rpc/stphandler.go

--- lacp/rpc/lahandler.go
+++ lacp/rpc/lahandler.go
@@ -459,7 +459,7 @@ func GetAddDelMembers(orig []uint16, update []int32) (add, del []int32) {
 	return
 }
 
-func (la LACPDServiceHandler) UpdateLaPortChannel(origconfig *lacpd.LaPortChannel, updateconfig *lacpd.LaPortChannel, attrset []bool, op string) (bool, error) {
+func (la LACPDServiceHandler) UpdateLaPortChannel(origconfig *lacpd.LaPortChannel, updateconfig *lacpd.LaPortChannel, attrset []bool, op []*lacpd.PatchOpInfo) (bool, error) {
 
 	aggModeMap := map[uint32]string{
 		//LacpActivityTypeACTIVE:  "ACTIVE",
