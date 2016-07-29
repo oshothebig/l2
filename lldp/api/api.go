67b8d528906437d4e5f0635d02a59068ab1b3515
Avoy Nanda <ananda@snaproute.com>
2016-07-28 23:45:00 -0700

    lldp intf discovery

--- lldp/api/api.go
+++ lldp/api/api.go
@@ -103,6 +103,11 @@ func SendPortStateChange(ifIndex int32, state string) {
 	lldpapi.server.IfStateCh <- &config.PortState{ifIndex, state}
 }
 
+func GetIntfs(idx int, cnt int) (int, int, []config.Intf) {
+	n, c, result := lldpapi.server.GetIntfs(idx, cnt)
+	return n, c, result
+}
+
 func GetIntfStates(idx int, cnt int) (int, int, []config.IntfState) {
 	n, c, result := lldpapi.server.GetIntfStates(idx, cnt)
 	return n, c, result
